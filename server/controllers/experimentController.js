ebb8343ca647518bd12c314175e98a544f01de9f
Hemanth Kapa <kapa4902@gmail.com>
2024-07-09 12:11:35 -0700

    just pushing for the sake of pushing

--- server/controllers/experimentController.js
+++ server/controllers/experimentController.js
@@ -15,17 +15,20 @@ exports.homepage = async (req, res) => {
     }
 
     let perPage = 12;
-    let page = req.query.pge || 1 ;
+    let page = req.query.page || 1 ;
 
 
     try{
-        const exp = await Experiment.aggregate([{$sort: { updatedAt: -1}}])
-            .skip(perPage * page - perPage)
-            .limit(perPage)
-            .exec();
-        const count = await experiment.count();
+        const count = await Experiment.countDocuments();
+        const exp = await Experiment.aggregate([
+            {$sort: {updatedAt: -1 }},
+            {$skip: perPage * (page - 1)},
+            {$limit: perPage}
+        ]).exec();
+        
 
-        res.render('index', {locals, 
+        res.render('index', {
+            locals, 
             exp,
             current: page, 
             pages: Math.ceil(count / perPage)
@@ -56,6 +59,7 @@ exports.addExperiment = async(req, res) => {
  */
 
 exports.postExperiment = async(req, res) => {
+    
 
     console.log(req.body);
 
@@ -74,6 +78,28 @@ exports.postExperiment = async(req, res) => {
     }catch (error){
         console.log(error);
     }
-    
-    
 }
+
+    /**
+     * GET /
+     * Experiment Data
+     */
+
+    exports.view = async (req, res) => {
+        try{
+            const experiment = await Experiment.findOne({_id: req.params.id })
+
+            const locals = {
+                title: "View experimet Data",
+                description: "A dashboard with tomographic expermient data"
+            };
+
+            res.render('experiment/view',{
+                locals,
+                experiment
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
--- server/routes/experiment.js
+++ server/routes/experiment.js
@@ -1,15 +1,16 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const experimentController = require('../controllers/experimentController');
 
 /**
- *  Experiment Routed
+ *  Experiment Routes
  */
 
 router.get('/', experimentController.homepage);
 
-
 router.get('/add', experimentController.addExperiment);
 router.post('/add', experimentController.postExperiment);
+
+router.get('/view/:id', experimentController.view);
 
 module.exports = router;
